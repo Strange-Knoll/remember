86ccf464f72d264be86d4d061297bb37f4b0aff4
Strange Knoll <strangeknoll@gmail.com>
2024-04-09 12:32:39 -0400

    clap rework

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use clap::*;
+use clap::{error::ContextValue, Command, *};
 use std::{
+    borrow::BorrowMut,
     env,
     fs::{self, File, OpenOptions},
     io::{stdout, Read, Seek, SeekFrom, Write},
@@ -12,66 +13,80 @@ use crossterm::{
 };
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    match args[1].as_str() {
-        "-n" | "--new" => {
-            assert!(args.len() > 2);
-            let content = &args[2];
-            println!("content: {}", content);
-            let mut file = OpenOptions::new()
-                .write(true)
-                .append(true)
-                .open("example.txt")
-                .unwrap();
+    //let args: Vec<String> = env::args().collect();
+    //_old(args);
 
-            //file.seek(From::End(0)).unwrap();
-            file.write_all(content.as_bytes()).unwrap();
-            file.write_all("\n".as_bytes()).unwrap();
-            let mut content = String::new();
+    let cmd = Command::new("reminder").args([
+        arg!(--list "list reminders"),
+        arg!(--new <note> "create reminder"),
+        arg!(--remove <num> "remove reminder"),
+    ]);
 
-            let mut file = File::open("./example.txt").unwrap();
-            file.read_to_string(&mut content);
-            print_line_number(content);
+    let matches = cmd.get_matches();
+
+    for m in matches.get_many::<bool>("list").unwrap() {
+        //println!("Matched ls");
+        if m == &true {
+            list()
         }
-        "-ls" | "--list" => {
-            let mut content = String::new();
-            let mut file = File::open("./example.txt").unwrap();
-            file.read_to_string(&mut content).unwrap();
-            print_line_number(content);
-        }
-        "-rm" | "--remove" => {
-            assert!(args.len() > 2);
-            let line: usize = args[2].parse::<usize>().unwrap();
+    }
 
-            let mut content = String::new();
-            let mut file = File::open("./example.txt").unwrap();
-            file.read_to_string(&mut content).unwrap();
-            let mut split: Vec<&str> = content.split('\n').collect();
-            split.remove(line);
-            let mut file = File::create("./example.txt").unwrap();
-            file.write_all(split.join("\n").as_bytes()).unwrap();
-
-            let mut file = File::open("./example.txt").unwrap();
-            let mut content = String::new();
-            file.read_to_string(&mut content);
-            print_line_number(content);
-        }
-        "-h" | "--help" => {}
-        _ => {}
+    for mut m in matches.get_many::<String>("new") {
+        //println!("Matched n");
+        let c = m.next().unwrap();
+        new(&mut c.to_string());
+        list();
+    }
+    for m in matches.get_one::<String>("remove") {
+        //println!("Matched rm");
+        let m = m.parse::<usize>().unwrap();
+        rm(&m);
+        list();
     }
 
     //pretty_print(&["Hello", "world!"], &[Color::White, Color::Magenta]);
+}
+
+fn list() {
+    let mut content = String::new();
+    let mut file = File::open("./example.txt").unwrap();
+    file.read_to_string(&mut content);
+    print_line_number(content);
+}
+
+fn new(s: &mut String) {
+    let mut file = OpenOptions::new().append(true).open("example.txt").unwrap();
+
+    s.push_str("\n");
+    file.write_all(s.as_bytes());
+    list();
+}
+
+fn rm(index: &usize) {
+    let mut content = String::new();
+    let mut file = File::open("example.txt").unwrap();
+    file.read_to_string(&mut content);
+    let mut split: Vec<&str> = content.split("\n").collect();
+    split.remove(*index);
+    //println!("{}", split);
+    let mut file = File::create("example.txt").unwrap();
+    file.write_all(split.join("\n").as_bytes());
 }
 
 fn print_line_number(s: String) {
     let split: Vec<&str> = s.split('\n').collect();
 
     for (i, s) in split.iter().enumerate() {
-        pretty_print(
-            &[format!("{}: ", i).as_str(), &s, "\n"],
-            &[Color::Green, Color::White, Color::White],
+        pretty_print_ln(
+            &[format!("{}:", i).as_str(), &s],
+            &[Color::Green, Color::White],
         )
     }
+}
+
+fn pretty_print_ln(strings: &[&str], colors: &[Color]) {
+    pretty_print(strings, colors);
+    println!("\n");
 }
 
 fn pretty_print(strings: &[&str], colors: &[Color]) {
